00e149f89a9af5b6e7e185aa7f70e774e6c352b2
andii04 <36232071+andii04@users.noreply.github.com>
2020-05-19 15:08:07 +0200

    dieses buildern pattern, keine ahnung

--- src/main/cruise_ship/CruiseShip.java
+++ src/main/cruise_ship/CruiseShip.java
@@ -6,6 +6,11 @@ public class CruiseShip {
     private String name = "Symphony of the Seas";
     private EventBus eventBus;
 
+    public CruiseShip(String name, EventBus eventBus) {
+        this.name = name;
+        this.eventBus = eventBus;
+    }
+
     public String getName() {
         return name;
     }
@@ -36,5 +41,36 @@ public class CruiseShip {
 
     private class Builder{
 
+        private String name = "Symphony of the Seas";
+        private EventBus eventBus;
+
+        public Builder(String name, EventBus eventBus) {
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setEventBus(EventBus eventBus) {
+            this.eventBus = eventBus;
+            return this;
+        }
+
+        public Builder boarding(){
+            return this;
+        }
+
+        public Builder notifyHospital(String message){
+            return this;
+        }
+
+        public Builder startSimulation(){
+            return this;
+        }
+
+        public Builder build() {
+            return new Builder(name,eventBus);
+        }
     }
 }
