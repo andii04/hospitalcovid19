7f228ae7cf05e7a4e0447ef448069fc025783f4e
DennisWohlfarth <den.wohlfarth.17@lehre.mosbach.dhbw.de>
2020-06-09 20:30:14 +0200

    Schauze voll für heute

--- src/main/cruise_ship/CabinDeck.java
+++ src/main/cruise_ship/CabinDeck.java
@@ -4,11 +4,11 @@ public class CabinDeck extends Deck {
 
     public CabinDeck(DeckID id){
         super(id);
-        Cabin[] cabins = new Cabin[450];
-        for (int i = 0; i<150; i++){
-            cabins[i] = new Cabin(CabinLocation.InnerCenter, "Deck: "+id.name()+ " Room number: "+ i);
-            cabins[i+150] = new Cabin(CabinLocation.OuterLeft, "Deck: "+id.name()+ " Room number: "+ i+150);
-            cabins[i+300] = new Cabin(CabinLocation.OuterRight, "Deck: "+id.name()+ " Room number: "+ i+300);
+        Cabin[] cabins = new Cabin[451];
+        for (int i = 1; i<=150; i++){
+            cabins[i] = new Cabin(CabinLocation.OuterLeft, id.name()+ "-OL-"+ i);
+            cabins[i+150] = new Cabin(CabinLocation.InnerCenter, id.name()+ "-IC-"+ i);
+            cabins[i+300] = new Cabin(CabinLocation.OuterRight, id.name()+ "-OR-"+ i);
         }
     }
 
