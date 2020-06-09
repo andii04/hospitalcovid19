c73ee0a54ddad1779e7e5342c8108188a305b281
andii04 <36232071+andii04@users.noreply.github.com>
2020-06-09 20:54:47 +0200

    LungCells

--- src/main/shared/LungCell.java
+++ src/main/shared/LungCell.java
@@ -1,4 +1,27 @@
 package shared;
 
 public class LungCell extends Cell {
+
+    char[]charpool = new char[3];
+    char[][][] lungCell = new char[3][3][3];
+
+    public void arraytest(){
+        for (int i = 0; i < lungCell.length; i++)
+        {
+            for (int a = 0; a < lungCell[i].length; a++)
+            {
+                for (int b = 0; b < lungCell[i][a].length; b++)
+                {
+                    //hier anpassen
+                    lungCell[i][a][b] = charpool[0];
+                }
+            }
+        }
+    }
+
+    public LungCell() {
+        charpool[0] = 'd';
+        charpool[1] = 'n';
+        charpool[2] = 'a';
+    }
 }
