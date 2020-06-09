ddfa17add94592ece9ef728604b29ef554cb8fbb
DennisWohlfarth <den.wohlfarth.17@lehre.mosbach.dhbw.de>
2020-06-09 13:01:51 +0200

    Müll

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,4 +1,33 @@
+import shared.Human;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Application {
     public static void main(String... args) {
+        createHumans();
+
+    }
+
+    public static boolean createHumans(){
+        File name = new File("data/name.txt");
+        File birthday = new File("data/birthdate.csv");
+        if (!name.canRead() || !name.isFile())
+            System.exit(0);
+
+        BufferedReader in = null;
+
+        try {
+            in = new BufferedReader(new FileReader("data/name.txt"));
+            String zeile = null;
+            while ((zeile = in.readLine()) != null) {
+                System.out.println("Gelesene Zeile: " + zeile);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 }
--- src/main/shared/Human.java
+++ src/main/shared/Human.java
@@ -14,6 +14,24 @@ public class Human {
     private boolean hasMouthProtection;
     private Clothing clothing;
 
+    public Human(String firstName, String lastName, String birthDate, Nationality nationality, boolean isSmoking, boolean hasAsthma, boolean hasHIV, boolean isInfectedCOVID19, boolean hasFever, boolean hasTaste, boolean hasMouthProtection, Clothing clothing){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.nationality =  nationality;
+        this.isSmoking = isSmoking;
+        this.hasAsthma = hasAsthma;
+        this.hasHIV = hasHIV;
+        this.isInfectedCOVID19 = isInfectedCOVID19;
+        this.hasFever = hasFever;
+        this.hasTaste = hasTaste;
+        this.hasMouthProtection = hasMouthProtection;
+        this.clothing = clothing;
+    }
+    public Human(){
+
+    }
+
     public void dress (Clothing clothing){
 
     }
@@ -34,8 +52,70 @@ public class Human {
 
     }
 
-    // inner class
-    private class Builder {
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private String birthDate;
+        private Nationality nationality;
+        private boolean isSmoking;
+        private boolean hasAsthma;
+        private boolean hasHIV;
+        private boolean isInfectedCOVID19;
+        private boolean hasFever;
+        private boolean hasTaste;
+        private boolean hasMouthProtection;
+        private Clothing clothing;
 
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public String getBirthDate() {
+            return birthDate;
+        }
+
+        public Nationality getNationality() {
+            return nationality;
+        }
+
+        public boolean isSmoking() {
+            return isSmoking;
+        }
+
+        public boolean isHasAsthma() {
+            return hasAsthma;
+        }
+
+        public boolean isHasHIV() {
+            return hasHIV;
+        }
+
+        public boolean isInfectedCOVID19() {
+            return isInfectedCOVID19;
+        }
+
+        public boolean isHasFever() {
+            return hasFever;
+        }
+
+        public boolean isHasTaste() {
+            return hasTaste;
+        }
+
+        public boolean isHasMouthProtection() {
+            return hasMouthProtection;
+        }
+
+        public Clothing getClothing() {
+            return clothing;
+        }
+
+        public Human build() {
+            return new Human(firstName, lastName, birthDate, nationality, isSmoking, hasAsthma, hasHIV, isInfectedCOVID19, hasFever, hasTaste, hasMouthProtection, clothing);
+        }
     }
 }
