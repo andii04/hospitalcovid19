a0b523b5c8f81f72ddf76afaca247232e000b75d
DennisWohlfarth <den.wohlfarth.17@lehre.mosbach.dhbw.de>
2020-06-10 21:05:28 +0200

    Hustet

--- src/main/shared/ImmuneSystem.java
+++ src/main/shared/ImmuneSystem.java
@@ -1,4 +1,27 @@
 package shared;
 
 public class ImmuneSystem {
+    Human human;
+
+    public ImmuneSystem(Human human){
+        this.human = human;
+    }
+    public void work(){
+        for(int lung= 0; lung <1; lung++){
+            for(int i=0; i<human.getLungs().get(lung).getStructure().length; i++){
+                for(int x= 0; x <human.getLungs().get(lung).getStructure()[0].length; x++){
+                    for(int y = 0; y <human.getLungs().get(0).getStructure()[0][0].length; y++){
+                        if(human.getLungs().get(0).getStructure()[i][x][y].getClass().toString().equals("InfectedCell")){
+                            System.out.println("JA");
+                        }
+                        else{
+                            System.out.println("Ne");
+                        }
+                    }
+                }
+            }
+        }
+        //human.getLungs().get(0).getStructure().length;
+
+    }
 }
--- src/main/shared/Lung.java
+++ src/main/shared/Lung.java
@@ -2,14 +2,20 @@ package shared;
 
 public class Lung {
     int nochmalneumachen = 500;
-    private LungCell[][][] structure;
+    private Cell[][][] structure;
     private char[] liquidParticle = new char[nochmalneumachen];
 
     public Lung(){
         structure  = new LungCell[25][10][2];
     }
 
-    public LungCell[][][] getStructure() {
+    public void createInfectedCell(char virus, int i, int x, int y){
+        //System.out.println(structure[i][x][y].);
+        //structure[i][x][y] = (InfectedCell) structure[i][x][y];
+
+    }
+
+    public Cell[][][] getStructure() {
         return structure;
     }
 }
